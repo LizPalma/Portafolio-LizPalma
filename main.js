f7b108e72c39d0d89ee510f6e3745c0d88e7f5f1
LizPalma <lgpalma15@gmail.com>
2023-08-19 01:37:31 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//boton menu
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+}) 
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+
+
+
+
